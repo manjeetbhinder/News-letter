7644465ac04f4238d50144f9607b5a3b8e86c13d
manjeetbhinder <bhinder3521@gmail.com>
2021-02-09 22:39:37 +0530

    my project

---
+++ app.js
@@ -0,0 +1,62 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const https=require("https");
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+})
+
+app.post("/",function(req,res){
+  const firstName=req.body.fName;
+  const lastName=req.body.lName;
+  const email=req.body.Email;
+  const data={
+    members:[
+      {
+        email_address:email,
+        status:"subscribed",
+        merge_fields:{
+          FNAME:firstName,
+          LNAME:lastName
+        }
+      }
+    ]
+  };
+  const jsonData=JSON.stringify(data);
+  const url="https://us19.api.mailchimp.com/3.0/lists/e40811ae8d";
+  const options={
+    method:"POST",
+    auth:"bhinder:9aa61b70ca0e8f36c3152002e275bd34-us19"
+  }
+  const request=https.request(url,options,function(response){
+    if(response.statusCode===200){
+      res.sendFile(__dirname+"/success.html");
+    }
+    else{
+      res.sendFile(__dirname+"/failure.html");
+    }
+    response.on("data",function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.post("/failure",function(req,res){
+  res.redirect("/");
+})
+
+app.listen(3000,function(){
+  console.log("Working perfectly");
+})
+
+// 9aa61b70ca0e8f36c3152002e275bd34-us19
+
+// e40811ae8d
